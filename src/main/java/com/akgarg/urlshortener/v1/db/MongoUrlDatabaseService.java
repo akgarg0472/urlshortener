37d211ed519c74e154e2fe1f557fe341f7352690
akgarg0472 <akgarg0472@gmail.com>
2025-02-25 01:58:29 +0530

    fix: Duplicate short URL insertion exception handling

--- src/main/java/com/akgarg/urlshortener/v1/db/MongoUrlDatabaseService.java
+++ src/main/java/com/akgarg/urlshortener/v1/db/MongoUrlDatabaseService.java
@@ -1,8 +1,10 @@
 package com.akgarg.urlshortener.v1.db;
 
+import com.akgarg.urlshortener.exception.UrlShortenerException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -24,8 +26,11 @@ public class MongoUrlDatabaseService implements UrlDatabaseService {
             }
             return true;
         } catch (Exception e) {
+            if (e instanceof DuplicateKeyException && e.getMessage().contains("E11000") && e.getMessage().contains("short_url")) {
+                throw new UrlShortenerException(new String[]{}, 409, "Short url already exists");
+            }
             log.error("Error saving url to database", e);
-            return false;
+            throw e;
         }
     }
 
